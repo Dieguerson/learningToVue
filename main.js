04b6bedf4c165bf4cfd6c1ee312dd8009f79288d
Dieguerson <voices113@hotmail.com>
2021-11-29 15:43:39 -0300

    Learing to Vue 1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+//Vue es reactive, cuando un valor cambia, cualquier cosa que depende de el cambia automáticamente.
+
+const app = Vue.createApp({//Options Object: No es opcional, pero permite añadir propiedades opcionales para configurar la app. Siempre tiene que estar, aunque sea vacío.
+    data: function(){//Option: propiedad del Option Object. Objeto con la data a utilizar.
+        return {
+            product: 'Socks',
+            description: 'A lovely pair of socks'
+        }
+    }
+})
