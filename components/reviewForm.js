7ceb93656e97480a25f3ccab842eafca6a31a3ff
Dieguerson <voices113@hotmail.com>
2021-12-01 09:44:47 -0300

    Forms and v-model

---
+++ components/reviewForm.js
@@ -0,0 +1,52 @@
+app.component("review-form" , {
+    template:
+    /*html*/
+        `<form class="review-form" @submit.prevent="onSubmit"> <!--Prevent es un modificador que evita el refresh default del browser-->
+            <h3>Leave a review</h3>
+            <label for="name">Name:</label><!--v-model permite ahcer binding de información en dos sentidos-->
+            <input id="name" v-model="name">
+
+            <label for="review">Review:</label>      
+            <textarea id="review" v-model="review"></textarea>
+
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating"><!--.number es una modificador que tipifica el valor como un número-->
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+
+            <label for="recommend">Would you recommend this product?</label>
+            <select id="recommend" v-model="recommend">
+                <option>Yes</option>
+                <option>No</option>
+            </select>
+
+            <input class="button" type="submit" value="Submit">
+         </form>`,
+         data(){
+             return {name: "" , review: "" , rating: null , recommend: null}
+         },
+         methods: {
+             onSubmit(){
+                 if(this.name === "" || this.review === "" || this.rating === null || recommend === null){
+                     alert("Review is incomplete. Please fill out every field.")
+                     return
+                 }
+                 let productReview = {
+                     name: this.name,
+                     review: this.review,
+                     rating: this.rating,
+                     recommend: this.recommend
+                 }
+                 this.$emit("review-submitted" , productReview)
+                 this.name = ""
+                 this.review = ""
+                 this.rating = null
+                 this.recommend = null
+             }
+         }
+         
+})
